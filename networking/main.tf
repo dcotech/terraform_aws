7c62821943b87cb7be65f8b0a170d94b8b9fb0a9
Damien ONeal <doneal@chef.io>
2018-12-23 22:01:10 -0500

    added subnets,security, and README

--- networking/main.tf
+++ networking/main.tf
@@ -50,3 +50,56 @@ resource "aws_default_route_table" "tf_private_rt" {
         Name = "tf_private"
     }
 }
+
+#Subnets
+
+resource "aws_subnet" "tf_public_subnet" {
+    #This was created to avoid repeatition. The simpler way is to make two public aws_subnet resources with different names, variables, etc
+    count = 2 #count up to two
+    vpc_id = "${aws_vpc.tf_vpc.id}"
+    cidr_block = "${var.public_cidrs[count.index]}"
+    map_public_ip_on_launch = true
+    availability_zone = "${data.aws_availability_zones.available.names[count.index]}"
+
+    tags {
+        Name = "tf_public_${count.index + 1}" #This is start at the number 1, since index start at 0
+    }
+}
+
+resource "aws_route_table_association" "tf_public_association" {
+  count = "${aws_subnet.tf_public_subnet.count}"
+  subnet_id = "${aws_subnet.tf_public_subnet.*.id[count.index]}"
+  route_table_id = "${aws_route_table.tf_public_rt.id}"
+}
+
+#Secure the environment. This isn't necessarily the best practice -- since an organization
+#would use additional hardning such as RBAC, 2-factor authentication, etc
+resource "aws_security_group" "tf_public_sg" {
+  name = "tf_public_sg"
+  description = "Used for access to public instances"
+  vpc_id = "${aws_vpc.tf_vpc.id}"
+
+  #SSH -- inbound traffic
+  ingress {
+      from_port = 22
+      to_port = 22
+      protocol = "tcp"
+      cidr_blocks = ["${var.localip}"] #if words are plural, in the TF world, much of the time it's a list
+  }
+
+   #HTTP -- inbound traffic
+  ingress {
+      from_port = 80
+      to_port = 80
+      protocol = "tcp"
+      cidr_blocks = ["${var.localip}"]
+  }
+
+  egress {
+      from_port = 0 #allow all ports
+      to_port = 0 
+      protocol = "-1" #allow all protocols: UDP, TCP, etc
+      cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
--- networking/variables.tf
+++ networking/variables.tf
@@ -2,3 +2,14 @@
 variable "vpc_cidr" {
   default = "10.123.0.0/16"
 }
+
+variable "public_cidrs" {
+  default = [
+      "10.124.1.0/24",
+      "10.124.2.0/24"
+  ]
+}
+
+variable "localip" {
+  default = "0.0.0.0/0"
+}
